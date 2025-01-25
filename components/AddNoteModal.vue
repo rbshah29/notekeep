1a2cc6a3b82bdd3b213702a4ffd07bd282b99eb9
Rutvik Shah <rbshah0029@gmail.com>
2025-01-24 22:53:25 -0500

    Updated auth in package
    - Formated add forms
    - Increase width fro note column

--- components/AddNoteModal.vue
+++ components/AddNoteModal.vue
@@ -1,6 +1,6 @@
 <template>
     <div v-if="showModal" class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center dark:bg-opacity-75">
-      <div class="bg-white dark:bg-gray-800 p-6 rounded-lg w-96">
+      <div class="bg-white dark:bg-gray-800 p-6 rounded-lg w-2/3">
         <h2 class="text-lg font-bold text-gray-900 dark:text-white mb-4">Take a note....</h2>
         <input v-model="newNote.title" placeholder="Title" class="w-full p-2 mb-4 border rounded dark:bg-gray-700 dark:text-white">
         <textarea v-model="newNote.content" placeholder="Content" class="w-full p-2 border rounded dark:bg-gray-700 dark:text-white"></textarea>
--- components/EditNoteModal.vue
+++ components/EditNoteModal.vue
@@ -1,6 +1,11 @@
 <template>
   <div v-if="show" class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
     <div class="bg-white dark:bg-gray-800 p-6 rounded-lg w-2/3 max-h-[80vh] overflow-y-auto">
+      <div class="flex justify-end mb-3">
+          <button @click="closeModal" class="px-4 py-2 rounded text-black dark:text-white">
+          <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="m15.5 15.5l-10-10zm0-10l-10 10"/></svg>
+        </button>
+      </div>
       <input 
         v-model="editedNote.title" 
         class="w-full p-2 mb-4 border rounded dark:bg-gray-700 dark:text-white text-xl font-bold"
@@ -12,7 +17,6 @@
       <div class="mt-4 flex justify-end space-x-2">
         
         <button @click="saveEdit" class="bg-blue-500 text-white px-4 py-2 rounded">Save</button>
-        <button @click="closeModal" class="bg-gray-300 dark:bg-gray-600 px-4 py-2 rounded text-black dark:text-white">Cancel</button>
       </div>
     </div>
   </div>
